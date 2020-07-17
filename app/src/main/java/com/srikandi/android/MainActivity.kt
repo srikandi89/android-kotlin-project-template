140cf5b6decdf6c3cb152c89cba0ab3c0641c6de
Otniel <otniel@ruangguru.com>
2020-07-17 19:45:34 +0700

    add description in product flavor, set textview id in main activity

--- app/src/main/java/com/srikandi/android/MainActivity.kt
+++ app/src/main/java/com/srikandi/android/MainActivity.kt
@@ -2,13 +2,13 @@ package com.srikandi.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("MAIN_ACTIVITY", "Host url ${BuildConfig.HOST_URL}")
+        textview_message.text = BuildConfig.HOST_URL
     }
 }
